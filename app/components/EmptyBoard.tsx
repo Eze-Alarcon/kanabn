16e20a6f0705d0ca39b638040d2e8cd3c80121c0
ezequiel <ps3ezequiel@gmail.com>
2023-06-27 18:20:09 +0200

    board part 3

--- app/components/EmptyBoard.tsx
+++ app/components/EmptyBoard.tsx
@@ -4,7 +4,7 @@ export default function EmptyBoard () {
   return (
     <div className='h-full grid place-content-center gap-6 justify-center w-2/3 mx-auto'>
       <p className='text-[#828FA3] text-base font-bold text-center'>This board is empty. Create a new column to get started.</p>
-      <button className='w-44 h-12 bg-kpurple mx-auto rounded-3xl text-kwhite font-bold'>+ Add New Column</button>
+      <button className='w-44 h-12 bg-kpurple mx-auto rounded-3xl text-kwhite font-bold hover:bg-kpurpleli'>+ Add New Column</button>
     </div>
   )
 }
