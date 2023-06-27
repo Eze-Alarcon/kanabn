1ff1ae539909d99ce1d4e5660e384b546b5164c7
Jerry <jerrygonzalezzambrano@gmail.com>
2023-06-27 16:09:46 +0200

    Cambios en header

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -2,15 +2,15 @@ import React from 'react'
 
 export default function HeaderComp() {
   return (
-<nav className="w-100% h-14 flex flex-row">
+<nav className="w-full h-[64px] flex flex-row">
       <div className="w-3/12 flex items-center justify-center">
-        <img className='w-7 h-7'  src="/styles/assets/logo-mobile.svg" alt="logo-mobile.svg" />
+        <img className='w-[34px] h-[35px]'  src="/styles/assets/logo-mobile.svg" alt="logo-mobile.svg" />
       </div>
-      <div className="w-8/12 flex items-center font-bold">
+      <div className="w-8/12 flex items-center font-bold text-xl">
         Platform Launch
       </div>
-      <div className="w-5/12 flex content-center items-center justify-evenly">
-        <div className='w-14 h-10 opacity-30 bg-kpurple rounded-full flex justify-center items-center'>
+      <div className="w-3/12 flex content-center items-center justify-evenly">
+        <div className='w-[48px] h-[32px] opacity-30 bg-kpurple hover:opacity-100 rounded-full flex justify-center items-center cursor-pointer duration-300'>
           <img className='w-3.5 h-3.5' src="styles/assets/icon-add-task-mobile.svg" alt="icon-add-task-mobile.svg" />
         </div>
         <div>
