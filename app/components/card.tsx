7f90522c13a8fb635cda5de2b02321ab2942de03
Jerry <jerrygonzalezzambrano@gmail.com>
2023-06-27 18:54:41 +0200

    component card

---
+++ app/components/card.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Card() {
+  return (
+    <>
+          <section className='bg-kwhite shadow-lg ml-5 mr-5 rounded-md mt-6'>
+              <div className='font-bold p-4'>Build UI for onboarding flow</div>
+              <div className='text-sm pl-4 pb-3 font-bold opacity-50'>0 de 3 Subtareas</div>
+          </section>
+    </>
+   
+  )
+}
