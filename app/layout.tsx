2436c7b179aedcc78286ddb3ac483aae4928e0f3
ezequiel <ps3ezequiel@gmail.com>
2023-06-29 11:31:43 +0200

    sidebar and merge

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable camelcase */
-"use client"
+'use client'
 import './globals.css'
 import { Plus_Jakarta_Sans } from 'next/font/google'
 import HeaderComp from './components/header'
@@ -8,24 +8,17 @@ import TabletModal from './components/tabletModal'
 
 const plusJakartaSans = Plus_Jakarta_Sans({ subsets: ['latin'] })
 
-export default function RootLayout ({children}: {children: ReactNode}) {
+export default function RootLayout ({ children }: {children: ReactNode}) {
+  const [modalTablet, setModalTablet] = useState(false)
 
-  const [modalTablet, setModalTablet] = useState(false);
-
-  const handleClick = () => 
-  {
-    if(modalTablet == false)
-      setModalTablet(true);
-    else
-      setModalTablet(false);
+  const handleClick = () => {
+    if (modalTablet === false) { setModalTablet(true) } else { setModalTablet(false) }
   }
+
   return (
     <html lang='en'>
-
       <body className={`${plusJakartaSans.className} flex flex-col h-[100svh]`}>
-        {
-           <TabletModal handleClick={handleClick} modalTable={modalTablet}/>
-        }
+        <TabletModal handleClick={handleClick} modalTable={modalTablet} />
         <HeaderComp handleClick={handleClick} />
         <main className='w-full h-full bg-kcianli px-4 pt-6'>
           {children}
